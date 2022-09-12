b4636529bc911fa57f197a3744c2bd5827717b67
vowatchka <vowatchka@mail.ru>
2022-09-12 13:30:57 +0300

    fix var name

--- nbconvert.py
+++ nbconvert.py
@@ -1,3 +1,8 @@
+"""
+Модуль для конвертации примеров решений практических задач из формата
+Jupyter Notebook в формат Markdown.
+"""
+
 import os
 
 
@@ -10,11 +15,11 @@ OUTPUT_DIR = "web"
 
 
 def main():
-    for dir in DIRS:
-        for file_name in os.listdir(dir):
+    for dir_ in DIRS:
+        for file_name in os.listdir(dir_):
             if os.path.splitext(file_name)[1].lower() == ".ipynb":
-                input_file = os.path.join(dir, file_name)
-                output_dir = os.path.join(OUTPUT_DIR, dir)
+                input_file = os.path.join(dir_, file_name)
+                output_dir = os.path.join(OUTPUT_DIR, dir_)
 
                 os.system(f'jupyter nbconvert "{input_file}" --to={OUT_FORMAT} --output-dir="{output_dir}"')
 
